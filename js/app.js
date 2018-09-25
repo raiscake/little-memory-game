9597ca3b1f81803e1500d044be0fcc2c04400a33
Raisa Yang <raisa.yang@outlook.com>
2018-09-24 23:10:54 -0500

    Make full deck

--- js/app.js
+++ js/app.js
@@ -2,26 +2,34 @@ const grid = document.querySelector('.game-grid');
 
 const gridCells = document.getElementsByClassName('game-grid-cell');
 
-const cardsList = ['astronaut', 'atom', 'question-mark', 'robot', 'rocket', 'saturn', 'shootingstar', 'telescope', 'ufo'];
+const cardsList = ['astronaut', 'atom', 'robot', 'rocket', 'saturn', 'shootingstar', 'telescope', 'ufo'];
 
-const totalCards = 16;
+const totalCards = cardsList.length * 2;
 
-function randomizeCards(cards) {
+function makeCardDeck(cards) {
+    let pair = cardsList.slice(0),
+        deck = cardsList.concat(pair);
+     return deck;   
+}
+
+const cardDeck = makeCardDeck(cardsList);
+
+function randomizeDeck(deck) {
     let i = 0,
         j = 0,
         temp = null;
 
-    for (let i = cards.length - 1; i > 0; i--) {
+    for (let i = deck.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1)),
-            temp = cards[i];
-        cards[i] = cards[j];
-        cards[j] = temp;
+            temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
     }
-    return(cards);    
+    return(deck);    
 }
 
-function attachCards() {
-    const randomCards = randomizeCards(cardsList); 
+function attachDeck() {
+    const randomCards = randomizeDeck(cardDeck); 
     const gridCell = document.createElement('li');
     for (card of randomCards) {
         let cardName = card;
