ddc08755350e503142b20a087a5e8986a94826ec
Raisa Yang <raisa.yang@outlook.com>
2018-09-24 22:34:02 -0500

    Add randomizer function

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const gridCell = document.getElementsByClassName('game-grid-cell');
+
+const cardsList = ['astronaut', 'atom', 'question-mark', 'robot', 'rocket', 'saturn', 'shootingstar', 'telescope', 'ufo'];
+
+const totalCards = 16;
+
+function randomizeCards(cards) {
+    let i = 0,
+        j = 0,
+        temp = null;
+
+    for (let i = cards.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)),
+            temp = cards[i];
+        console.log(j);    
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+    console.log(cards);    
+}
