011337b665c68f378dc3cb8c287e342e2f793708
Raisa Yang <raisa.yang@outlook.com>
2018-09-24 23:00:47 -0500

    Add class to facedown cards (wip)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-const gridCell = document.getElementsByClassName('game-grid-cell');
+const grid = document.querySelector('.game-grid');
+
+const gridCells = document.getElementsByClassName('game-grid-cell');
 
 const cardsList = ['astronaut', 'atom', 'question-mark', 'robot', 'rocket', 'saturn', 'shootingstar', 'telescope', 'ufo'];
 
@@ -12,9 +14,19 @@ function randomizeCards(cards) {
     for (let i = cards.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1)),
             temp = cards[i];
-        console.log(j);    
         cards[i] = cards[j];
         cards[j] = temp;
     }
-    console.log(cards);    
+    return(cards);    
+}
+
+function attachCards() {
+    const randomCards = randomizeCards(cardsList); 
+    const gridCell = document.createElement('li');
+    for (card of randomCards) {
+        let cardName = card;
+        for (cell of gridCells) {
+            cell.classList.add(cardName);
+        }
+    }
 }
